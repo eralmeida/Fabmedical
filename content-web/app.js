0a4d803b00d9e74272c6bc4f281efa391da71f7f
eralmeida <eduardoalmeida@campus.ul.pt>
2022-02-17 17:53:43 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("d562ed37-6a77-40de-94c7-66393c83f04d");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
